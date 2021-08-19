7cb7ef7fa03bd600ff82bacc91f3988b800d1fff
Jonathan Souza <souzajonathan570@gmail.com>
2021-08-19 12:37:09 -0300

    files with the exercises of the block

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+let ingredients = [
+  {
+    ing1: 'Penne',
+    ing2: 'Alho',
+    qtd1: '500g',
+    qtd2: '50g',
+  }
+]
+
+console.log(ingredients);
---
+++ test.js
@@ -0,0 +1,12 @@
+let ingredients = [
+  {
+    ing1: 'Penne',
+    ing2: 'Alho',
+    qtd1: '500g',
+    qtd2: '50g',
+  }
+]
+
+console.log(ingredients.recude((acc, curr) => {
+  acc[curr] = curr
+}, {}));
